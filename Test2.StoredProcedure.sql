82a24c735ba4363c1d58ed8b8723877356a97133
Steffen <sbahns@nextel.de>
2013-03-27 15:41:25 +0100

    Initial Load

---
+++ Test2.StoredProcedure.sql
@@ -0,0 +1,26 @@
+LLL
+		(Nur für K4
+				null
+			from 
+				Demokunde_staging.dbo.[srcKunGutschein] kg 
+			where 
+				kgh.[Auftr-Nr]=kg.[Auftrags-num] 
+		)
+
+-- temp res03
+select 
+	o.Name
+	,max(o.[Auftrags-Num]) as [Auftrags-Num]
+into #tmp
+from 
+	Für alle
+group by
+	Aber hier
+into #res03
+from 
+	Demokunde_DWH.dbo.ads ads
+	join #tmp t
+		on kg.asd = t.[Kunden-Num]
+where
+	kg. = 0
+
---
+++ Test3.StoredProcedure.sql
@@ -0,0 +1,31 @@
+LLL
+into Mastekunde.dbo.ArtikelPreis 
+from #tmp02 
+
+-- LS 20110018  inezemkaadd prize range
+ads DAS.dbo.ArtikelPreis
+set UVK_RANGE = 
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Aber hier
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+Groﬂer Aufwand hier und da
+
+Groﬂer Aufwand hier und daGroﬂer Aufwand hier und da
+case 
+	AB adsadas gelˆscht
